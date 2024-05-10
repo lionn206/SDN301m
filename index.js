ab469536f1a36d2a57a3be1211f06e281e5867e6
lionn206 <luund206@gmail.com>
2024-05-10 15:55:49 +0700

    ex3-1

--- index.js
+++ index.js
@@ -1,25 +1,15 @@
-var rect = {
-  perimeter: (x, y) => 2 * (x + y),
-  area: (x, y) => x * y,
-};
+const http = require("http");
 
-function solveRect(l, b) {
-  console.log("Solving for rectangle with l = " + l + " and b = " + b);
+const hostname = "localhost";
+const port = 3000;
 
-  if (l <= 0 || b <= 0) {
-    console.log(
-      "Rectangle dimensions should be greater than zero:  l = " +
-        l +
-        ",  and b = " +
-        b
-    );
-  } else {
-    console.log("The area of the rectangle is " + rect.area(l, b));
-    console.log("The perimeter of the rectangle is " + rect.perimeter(l, b));
-  }
-}
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(-3,5);
+const server = http.createServer((req, res) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end("<html><body><h1>Hello, World!</h1></body></html>");
+});
 
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
