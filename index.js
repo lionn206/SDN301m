f8d10f1c651f5f5e9c8da0576dd71579966e8c4a
lionn206 <luund206@gmail.com>
2024-05-14 19:49:50 +0700

    Express Serve Static Files

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
   console.log(req.headers);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
 });
 
 const server = http.createServer(app);
