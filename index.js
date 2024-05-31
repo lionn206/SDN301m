9771a20167be5cf0f49e360f11357ff55ffa8223
hnil206 <luundde170719@fpt.edu.vn>
2024-05-31 16:57:32 +0700

    ex13

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
+const express = require('express');
+
 const MongoClient = require("mongodb").MongoClient;
 const assert = require("assert");
+const app = express();
 const url = "mongodb://localhost:27017/conFusion";
 const Dishes = require("./models/dishes");
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const dishRouter = require('./routes/dishRouter');
+const port = 3000;
 const connect = mongoose.connect(url);
 
-connect.then((db) => {
-
-    console.log('Connected correctly to server');
-
-    var newDish = Dishes({
-        name: 'Uthappizza1',
-        description: 'test1'
-    });
-
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.deleteMany({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-
+connect.then(
+  (db) => {
+    console.log("Connected correctly to server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+app.use('/dishes', dishRouter);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
 });
